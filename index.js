2298ae4fb2eff22f1aa662d148cc6bbcaca54076
Jordan Oliver <gorilla-san@pop-os.localdomain>
2022-10-09 13:32:48 +0200

    console.log apparently IS NOT A FUCKING FUNCTION

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const console = document.querySelector("#console")
 const sensor = document.querySelector("#sensor")
+const addBook = document.querySelector('#add-book')
+const addBookForm = document.querySelector('#add-book-form')
+const addButton = document.querySelector('#add')
+
+
+const booksStorage = []
+class book {
+    constructor(title, author, bookmark, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.bookmark = bookmark;
+        this.readStatus = readStatus;
+    }
+}
+
+
 
 function consoleRun() {
     var beep = new Audio('./sounds/beep.mp3');
@@ -20,3 +36,42 @@ function consoleRun() {
     }
    
 }
+
+function AddToCollection () {
+    if (addBook.classList.contains("add-book-open")) {
+        addBookForm.classList.add('add-book-form-closed')
+        addBookForm.classList.remove('add-book-form-open')
+        addBook.classList.add("add-book-closed")
+        addBook.classList.remove("add-book-open")
+        addButton.classList.add('add-button-closed')
+        addButton.classList.remove('add-button-open')
+        addButton.innerHTML = "Add to </br> collection"
+    }
+    else {
+        addBook.classList.remove("add-book-closed")
+        addBook.classList.add("add-book-open")
+        addButton.classList.remove('add-button-closed')
+        addButton.classList.add('add-button-open')
+        setTimeout(() => {
+        addButton.innerHTML = "Actually</br>&larr; no"
+        }, 1100);
+        setTimeout(function() {
+            addBookForm.classList.remove('add-book-form-closed')
+            addBookForm.classList.add('add-book-form-open')
+        }, 1100)
+
+    }
+}
+
+addBookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let bookmark = document.querySelector('#bookmark').value;
+    let readStatus = document.querySelector('input[name="status"]:checked').value;
+    const newBook = new book(title, author, bookmark, readStatus);
+    booksStorage.push(newBook);
+})
+
+
+
